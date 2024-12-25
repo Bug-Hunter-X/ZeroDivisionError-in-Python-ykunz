62ededd0bf3fa0d25ea4e73bbcef1efcf4072990
BugHunter-X <askzenai@gmail.com>
2024-12-25 08:36:03 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,11 @@
+def function(a, b):
+    if b == 0:
+        return "Division by zero!" 
+    else:
+        return a / b
+
+result = function(10, 0)
+print(result)
+
+result = function(10, 2)
+print(result) 
